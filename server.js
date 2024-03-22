8941c5d358654f240c5f1956b2dd88c56f52e260
blaz2f <46502527+blaz2f@users.noreply.github.com>
2024-03-22 11:43:17 +1100

    removing body-parser library

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 //let db = require("./dbConnection");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const routes = require('./routes')
 routes(app)
