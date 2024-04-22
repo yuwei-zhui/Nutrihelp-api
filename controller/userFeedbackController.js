387c076cb16c71167dea8f7ae21be85c3d1a26d9
sicsixdkn <150305764+sicsixdkn@users.noreply.github.com>
2024-04-23 09:03:23 +1000

    Adjusted error messages

--- controller/userFeedbackController.js
+++ controller/userFeedbackController.js
@@ -4,19 +4,19 @@ const userfeedback = async (req, res) => {
   try {
     const { name, contact_number, email, experience, message } = req.body;
     if (!name) {
-      return res.status(400).send('Name is required');
+      return res.status(400).send({ error: 'Name is required' });
     }
 
     if (!email) {
-      return res.status(400).send('Email is required');
+      return res.status(400).send({ error: 'Email is required' });
     }
 
     if (!experience) {
-      return res.status(400).send('Experience is required');
+      return res.status(400).send({ error: 'Experience is required' });
     }
     
     if (!message) {
-      return res.status(400).send('Message is required');
+      return res.status(400).send({ error: 'Message is required' });
     }
     
     await addUserFeedback(name, contact_number, email, experience, comments)
@@ -24,7 +24,7 @@ const userfeedback = async (req, res) => {
     res.status(201).json({ message: 'Data received successfully!' });
   } catch (error) {
     console.error({ error: 'error' });
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
 
