68d1069288fe47d3bcd9492a81df03818a22cfcf
sicsixdkn <150305764+sicsixdkn@users.noreply.github.com>
2024-04-26 15:41:24 +1000

    Adjusted endpoints and naming to use fooddata instead of preferences as it

--- routes/fooddata.js
+++ routes/fooddata.js
@@ -1,6 +1,6 @@
 const express    = require("express");
 const router     = express.Router();
-const controller = require("../controller/foodPreferencesController");
+const controller = require("../controller/foodDataController");
 
 
 router.route("/dietaryrequirements").get(controller.getAllDietaryRequirements);
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@ module.exports = app => {
     app.use("/api/contactus", require('./contactus'));
     app.use("/api/userfeedback", require('./userfeedback'));
     app.use("/api/appointments", require('./appointment'));
-    app.use("/api/foodpreferences", require('./foodpreferences'));
+    app.use("/api/fooddata", require('./fooddata'));
 };
