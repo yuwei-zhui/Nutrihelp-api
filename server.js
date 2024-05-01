f8142a395c66e6cdb0e0d68c028e29c3c54d9304
blaz2f <46502527+blaz2f@users.noreply.github.com>
2024-05-02 00:43:11 +1000

    migration to 80 port and adding cors library for connection to localhost

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 let db = require("./dbConnection");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.options("*", cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: "http://localhost:3000"}));
 
 const routes = require('./routes')
 routes(app)
