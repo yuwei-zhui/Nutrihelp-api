e071e8f715326da12a3fd977f3cb79d7d82a67d4
sicsixdkn <150305764+sicsixdkn@users.noreply.github.com>
2024-05-03 12:14:07 +1000

    User preferences get/put + more

--- model/fetchAllCookingMethods.js
+++ model/fetchAllCookingMethods.js
@@ -2,43 +2,15 @@ const supabase = require('../dbConnection.js');
 
 async function fetchAllCookingMethods() {
     try {
-        // TODO query database here when implemented
-        let dummyData = [
-            {
-                "id": 1,
-                "name": "Bake"
-            },
-            {
-                "id": 2,
-                "name": "Boil"
-            },
-            {
-                "id": 3,
-                "name": "Fry"
-            },
-            {
-                "id": 4,
-                "name": "Grill"
-            },
-            {
-                "id": 5,
-                "name": "Roast"
-            },
-            {
-                "id": 6,
-                "name": "Sauté"
-            },
-            {
-                "id": 7,
-                "name": "Simmer"
-            },
-            {
-                "id": 8,
-                "name": "Steam"
-            }
-        ]
+        let { data, error } = await supabase
+            .from('cooking_methods')
+            .select('*');
 
-        return dummyData;
+        if (error) {
+            throw error;
+        }
+
+        return data;
     } catch (error) {
         throw error;
     }
