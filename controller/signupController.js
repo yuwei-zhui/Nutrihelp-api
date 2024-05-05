f2355628a5e35d1a9b7993d6e01eac5abfdc4653
angeloremmilbautista <angeloremmilbautista@gmail.com>
2024-05-06 04:21:42 +1000

    added email into signupcontroller

--- controller/signupController.js
+++ controller/signupController.js
@@ -4,11 +4,11 @@ let addUser = require('../model/addUser.js')
 
 
 const signup = async (req, res) => {
-    const { username, password, contact_number } = req.body;
+    const { username, password, email, contact_number } = req.body;
 
     try {
-        if (!username || !password || !contact_number) {
-            return res.status(400).json({ error: 'Username, password, and contact number are required' });
+        if (!username || !password || !email || !contact_number) {
+            return res.status(400).json({ error: 'Username, password, email and contact number are required' });
         }
 
         const userExists = await getUser(username);
@@ -19,7 +19,7 @@ const signup = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        await addUser(username, hashedPassword, contact_number)
+        await addUser(username, hashedPassword, email, contact_number)
 
         return res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
