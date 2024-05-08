9ff6da67b5ee239e8295d186ca3ac0531b26748d
angeloremmilbautista <angeloremmilbautista@gmail.com>
2024-05-09 01:15:33 +1000

    fix with signupController with addUser

--- model/addUser.js
+++ model/addUser.js
@@ -4,7 +4,12 @@ async function addUser(username, password, mfa_enabled, contact_number) {
     try {
         let { data, error } = await supabase
             .from('users')
-            .insert({ username: username, password: password, mfa_enabled: mfa_enabled, contact_number: contact_number  })
+            .insert({ 
+              username: username,
+              password: password,
+              mfa_enabled: mfa_enabled,
+              contact_number: contact_number 
+            })
         return data
     } catch (error) {
         throw error;
