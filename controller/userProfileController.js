c60d2c9baedd87ae727fec85c26d9067652a38ca
Eric Studley <studley11@live.com.au>
2024-08-22 20:50:33 +1000

    Added unit tests for user feedback, user profile and user preferences

--- controller/userProfileController.js
+++ controller/userProfileController.js
@@ -1,41 +1,41 @@
-let updateUser = require('../model/updateUserProfile.js')
-let getUser = require('../model/getUserProfile.js')
+let updateUser = require("../model/updateUserProfile.js");
+let getUser = require("../model/getUserProfile.js");
 
 const updateUserProfile = async (req, res) => {
-  try {
-    if (!req.body.username) {
-      return res.status(400).send('Username is required');
-    }
+	try {
+		if (!req.body.username) {
+			return res.status(400).send("Username is required");
+		}
 
-    const user_profile = await updateUser(
-      req.body.username, 
-      req.body.first_name, 
-      req.body.last_name, 
-      req.body.email, 
-      req.body.contact_number
-    )
+		const user_profile = await updateUser(
+			req.body.username,
+			req.body.first_name,
+			req.body.last_name,
+			req.body.email,
+			req.body.contact_number
+		);
 
-    res.status(200).json(user_profile);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
+		res.status(200).json(user_profile);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Internal server error" });
+	}
 };
 
 const getUserProfile = async (req, res) => {
-  try {
-    const { username } = req.body;
-    if (!username) {
-      return res.status(400).send('Username is required');
-    }
+	try {
+		const { username } = req.body;
+		if (!username) {
+			return res.status(400).send("Username is required");
+		}
 
-    const userprofile = await getUser(username);
+		const userprofile = await getUser(username);
 
-    res.status(200).json(userprofile);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
+		res.status(200).json(userprofile);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Internal server error" });
+	}
 };
 
 module.exports = { updateUserProfile, getUserProfile };
