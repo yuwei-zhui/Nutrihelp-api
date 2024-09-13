8c6dbaf5e32f25f04a81c7ec1c987f8458bf58b4
blaz2f <46502527+blaz2f@users.noreply.github.com>
2024-09-13 13:38:45 +1000

    update messages

--- controller/userPasswordController.js
+++ controller/userPasswordController.js
@@ -5,15 +5,15 @@ let getUser = require("../model/getUserPassword.js");
 const updateUserPassword = async (req, res) => {
     try {
         if (!req.body.user_id) {
-            return res.status(400).send("User ID is required");
+            return res.status(400).send({ message:"User ID is required"});
         }
 
         if (!req.body.password) {
-            return res.status(400).send("Current password is required");
+            return res.status(400).send({ message:"Current password is required"});
         }
 
         if (!req.body.new_password) {
-            return res.status(400).send("New password is required");
+            return res.status(400).send({ message:"New password is required"});
         }
 
         const user = await getUser(req.body.user_id);
