9351b5ec03b6544a4a6fa9476279b0063106283c
Madhavi <madhavigupta2004@gmail.com>
2025-04-20 22:10:39 +1000

    input validation implemented for contact us endpoint

--- routes/login.js
+++ routes/login.js
@@ -1,35 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const controller = require('../controller/loginController.js');
-
-// router.route('/').post(function(req,res) {
-//     controller.login(req, res);
-// });
-// router.route('/mfa').post(function(req,res) {
-//     controller.loginMfa(req, res);
-// });
-// module.exports = router;
-
-// const express = require("express");
-// const router = express.Router();
-// const controller = require('../controller/loginController.js');
-
-// // Import validation rules and middleware
-// const { loginValidator, mfaloginValidator  } = require('../validators/loginValidator');
-// const validate = require('../middleware/validateRequest');
-
-// // POST /login
-// router.route('/').post(loginValidator, validate, (req, res) => {
-//     controller.login(req, res);
-// });
-
-// // POST /login/mfa
-// router.route('/mfa').post(mfaloginValidator, validate, (req, res) => {
-//     controller.loginMfa(req, res);
-// });
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const controller = require('../controller/loginController.js');
--- routes/signup.js
+++ routes/signup.js
@@ -1,13 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const controller = require('../controller/signupController.js');
-
-// router.route('/').post(function(req,res) {
-//     controller.signup(req, res);
-// });
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const controller = require('../controller/signupController.js');
