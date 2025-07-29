5139d10501b006c75c5b1b9e3e07403c2b631cb4
Abhimanyu Vijayakumar <manyu@Abhimanyus-MacBook-Pro.local>
2025-07-29 16:24:30 +0530

    All Support Functions in separate file

--- controller/estimatedCostController.js
+++ controller/estimatedCostController.js
@@ -110,7 +110,7 @@ const getPartialCost = async (req, res) => {
     };
 
     // Prepare response data
-    const { estimatedCost, lowPriceID, highPriceID } = prepareResponseData(lowPriceRequiredIngredients, highPriceRequiredIngredients);
+    const { estimatedCost, lowPriceID, highPriceID } = getEstimatedCost.prepareResponseData(lowPriceRequiredIngredients, highPriceRequiredIngredients);
 
     // Check if missing ingredient
     if(lowPriceID.length < ingredients.id.length || highPriceID.length < ingredients.id.length){
@@ -129,60 +129,6 @@ const getPartialCost = async (req, res) => {
       error: "Internal server error"
     })
   }
-}
-
-// Support function
-function prepareResponseData(lowPriceRequiredIngredients, highPriceRequiredIngredients) {
-  const estimatedCost = {
-    info: {
-      estimation_type: "",
-      include_all_wanted_ingredients: true,
-      minimum_cost: 0,
-      maximum_cost: 0
-    },
-    low_cost: {
-      price: 0,
-      count: 0,
-      ingredients: []
-    },
-    high_cost: {
-      price: 0,
-      count: 0,
-      ingredients: []
-    }
-  };
-  
-  let lowPriceID = [], highPriceID = [];
-  lowPriceRequiredIngredients.forEach((ingre) => {
-    estimatedCost.low_cost.ingredients.push({
-      ingredient_id: ingre.ingredient_id,
-      product_name: ingre.name,
-      quantity: ingre.estimation.unit + ingre.estimation.measurement,
-      purchase_quantity: ingre.estimation.purchase,
-      total_cost: ingre.estimation.total_cost
-    })
-    estimatedCost.info.minimum_cost += ingre.estimation.total_cost;
-    lowPriceID.push(ingre.ingredient_id);
-  })
-  highPriceRequiredIngredients.forEach((ingre) => {
-    estimatedCost.high_cost.ingredients.push({
-      ingredient_id: ingre.ingredient_id,
-      product_name: ingre.name,
-      quantity: ingre.estimation.unit + ingre.estimation.measurement,
-      purchase_quantity: ingre.estimation.purchase,
-      total_cost: ingre.estimation.total_cost
-    })
-    estimatedCost.info.maximum_cost += ingre.estimation.total_cost;
-    highPriceID.push(ingre.ingredient_id);
-  })
-  estimatedCost.info.minimum_cost = Math.round(estimatedCost.info.minimum_cost);
-  estimatedCost.info.maximum_cost = Math.round(estimatedCost.info.maximum_cost);
-
-  estimatedCost.low_cost.price =  estimatedCost.info.minimum_cost;
-  estimatedCost.low_cost.count =  estimatedCost.low_cost.ingredients.length;
-  estimatedCost.high_cost.price = estimatedCost.info.maximum_cost;
-  estimatedCost.high_cost.count =  estimatedCost.high_cost.ingredients.length;
-  return { estimatedCost, lowPriceID, highPriceID };
 }
 
 module.exports = {
--- model/getEstimatedCost.js
+++ model/getEstimatedCost.js
@@ -118,9 +118,63 @@ function estimateIngredientsCost(ingredients, ingredients_price) { //return grou
   };
 }
 
+function prepareResponseData(lowPriceRequiredIngredients, highPriceRequiredIngredients) {
+  const estimatedCost = {
+    info: {
+      estimation_type: "",
+      include_all_wanted_ingredients: true,
+      minimum_cost: 0,
+      maximum_cost: 0
+    },
+    low_cost: {
+      price: 0,
+      count: 0,
+      ingredients: []
+    },
+    high_cost: {
+      price: 0,
+      count: 0,
+      ingredients: []
+    }
+  };
+  
+  let lowPriceID = [], highPriceID = [];
+  lowPriceRequiredIngredients.forEach((ingre) => {
+    estimatedCost.low_cost.ingredients.push({
+      ingredient_id: ingre.ingredient_id,
+      product_name: ingre.name,
+      quantity: ingre.estimation.unit + ingre.estimation.measurement,
+      purchase_quantity: ingre.estimation.purchase,
+      total_cost: ingre.estimation.total_cost
+    })
+    estimatedCost.info.minimum_cost += ingre.estimation.total_cost;
+    lowPriceID.push(ingre.ingredient_id);
+  })
+  highPriceRequiredIngredients.forEach((ingre) => {
+    estimatedCost.high_cost.ingredients.push({
+      ingredient_id: ingre.ingredient_id,
+      product_name: ingre.name,
+      quantity: ingre.estimation.unit + ingre.estimation.measurement,
+      purchase_quantity: ingre.estimation.purchase,
+      total_cost: ingre.estimation.total_cost
+    })
+    estimatedCost.info.maximum_cost += ingre.estimation.total_cost;
+    highPriceID.push(ingre.ingredient_id);
+  })
+  estimatedCost.info.minimum_cost = Math.round(estimatedCost.info.minimum_cost);
+  estimatedCost.info.maximum_cost = Math.round(estimatedCost.info.maximum_cost);
+
+  estimatedCost.low_cost.price =  estimatedCost.info.minimum_cost;
+  estimatedCost.low_cost.count =  estimatedCost.low_cost.ingredients.length;
+  estimatedCost.high_cost.price = estimatedCost.info.maximum_cost;
+  estimatedCost.high_cost.count =  estimatedCost.high_cost.ingredients.length;
+  return { estimatedCost, lowPriceID, highPriceID };
+}
+
 module.exports = {
   getRecipeIngredients,
   getIngredientsPrice,
   convertUnits,
   estimateIngredientsCost,
+  prepareResponseData,
 }
